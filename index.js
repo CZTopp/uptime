de69a94f60991ed95ba470485073de9c37eefd66
AlQalamX <phmgllc@gmail.com>
2020-02-23 16:20:11 -0500

    parse the request and get trimmed path name

--- index.js
+++ index.js
@@ -4,10 +4,26 @@
 
 //Dependencies
 const http = require('http');
+const url = require('url');
 
 //server to respond to all requests
 const server = http.createServer((req, res) => {
+
+  //get url aßnd parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  //get the path from the url
+  var path = parsedUrl.pathname;
+
+  //trim extraneous slashes from both sides
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  //send response
   res.end('Welcome to Uptime\n');
+
+  //log the path requested
+  console.log('Request received on path: ' + trimmedPath);
+
 });
 //start server listen on port
 server.listen(3000, () => {
